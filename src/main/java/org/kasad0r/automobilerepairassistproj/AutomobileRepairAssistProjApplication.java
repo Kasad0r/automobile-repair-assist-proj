43525cd00d8979626a1bc1f374111c696cc53da4
= <dimon.pokemon99@gmail.com>
2020-07-03 17:51:01 +0200

    Initial commit

---
+++ src/main/java/org/kasad0r/automobilerepairassistproj/AutomobileRepairAssistProjApplication.java
@@ -0,0 +1,13 @@
+package org.kasad0r.automobilerepairassistproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileRepairAssistProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomobileRepairAssistProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kasad0r/automobilerepairassistproj/AutomobileRepairAssistProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kasad0r.automobilerepairassistproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileRepairAssistProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
